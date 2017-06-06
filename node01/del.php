9efb010501ab044333b33061f0b892978dcab9b2
shelling <1285972857@qq.com>
2017-06-06 21:21:23 +0800

    删除处理PHP页面
    1.处理删除数据，返回前端一个结果（echo 1 ）
         2.前端根据返回的数据1执行隐藏方法

--- node01/del.php
+++ node01/del.php
@@ -8,7 +8,9 @@ $conn =mysqli_connect($servername, $username, $password, $dbname);
 
 $id = $_GET['id'];
 $sql = "delete from user where id={$id}";
+//前端根据返回的数据1执行隐藏方法
 if (mysqli_query($conn,$sql)) {
+	//处理删除数据，返回前端一个结果（echo 1）
 	echo 1;
 }
 ?>
